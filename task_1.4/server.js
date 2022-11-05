60daf016992c18db3ab93e48fe8f2bfc9981d796
Mikhail <mihkailmatveichuk@gmail.com>
2022-11-05 17:26:10 +0300

    feat: complete task_1_4

--- task_1.4/server.js
+++ task_1.4/server.js
@@ -12,29 +12,6 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.end(message);
   }
-  
-
- 
-
-
-  
-
-
-
-
-
-  /* 
-    TODO: напишите обработчик запроса.
-    
-    1. Ответом на запрос к /?message=<text> должна быть строка <text>.
-       Статус ортвета - 200 (OK)
-    2. Если параметр message не задан, в ответ должна быть выведена подсказка: "Передайте строку в параметре message GET-запроса".
-       Статус ответа - 400 (BAD REQUEST)
-
-    Подсказка: используйте поле searchParams в объекте url для доступа к параметрам GET-запроса
-  */
-
-
 });
 
 module.exports = { server };
